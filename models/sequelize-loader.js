73cf76a0a3ea92bf0343999202551a7c823af42e
kkawamot <kkawamot@42tokyo.jp>
2021-09-20 19:09:57 +0900

    add dialectOptions

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -2,7 +2,17 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/url_fuck',
-  { logging: true });
+  {
+	  logging: true,
+	  dialect: 'postgres',
+	  dialectOptions: {//以下を追加
+		ssl: {
+			require: true,
+			rejectUnauthorized: false
+			}
+		}
+	}
+);
 
 module.exports = {
   database: sequelize,
